50091bac94d643da446c77e80c82997dc6323f6e
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-12-24 14:25:05 -0800

    gsap animation added

--- src/components/ModelLoader/index.tsx
+++ src/components/ModelLoader/index.tsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useLoader, useFrame, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import gsap from "gsap";
 
 const ModelLoader = () => {
   const { raycaster } = useThree();
   const [hovered, setHovered] = useState(null);
+  const rotationRef = useRef(null);
   const [originalColor, setOriginalColor] = useState(null);
   const gltf = useLoader(GLTFLoader, "/nissan1.glb", (loader) => {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.3/");
     loader.setDRACOLoader(dracoLoader);
   });
+
+  const handleClick = () => {
+    if (rotationRef.current) {
+      rotationRef.current.paused(true);
+      setTimeout(() => {
+        rotationRef.current.paused(false);
+      }, 5000);
+    }
+  };
+
+  useEffect(() => {
+    rotationRef.current = gsap.to(gltf.scene.rotation, {
+      y: "+=6.283",
+      duration: 55,
+      repeat: -1,
+      ease: "linear",
+      paused: false,
+    });
+  }, [gltf.scene]);
 
   useFrame(() => {
     const intersects = raycaster.intersectObject(gltf.scene, true);
@@ -38,7 +59,7 @@ const ModelLoader = () => {
 
   return (
     <group>
-      <primitive object={gltf.scene} />
+      <primitive object={gltf.scene} onClick={handleClick} />
     </group>
   );
 };
