1dc925294e23082e42a8ed5c3b9ed25188d92aea
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2024-01-01 22:59:44 -0800

    color- mixer added

--- src/components/ColorsContainer/index.tsx
+++ src/components/ColorsContainer/index.tsx
@@ -3,18 +3,28 @@ import { useAtom, useSetAtom } from "jotai";
 import { ColorsAtom } from "../../atoms/colors.atom";
 import { SelectedColorAtom } from "../../atoms/color.atom";
 import { MainContainer, Color, ColorContainer, IconPallete } from "./index.styled";
+import { ColorPicker, useColor, IColor } from "react-color-palette";
+import "react-color-palette/css";
 
 const colors = ["#ff0000", "#FFFFFF", "#800080", "#FFA500", "#FFFF00", "#00FF00"];
 
 const ColorsContainer = () => {
+  const [color, setMeshColor] = useColor("#123123");
+
   const [colorsShow, setColorsShow] = useAtom(ColorsAtom);
   const setColor = useSetAtom(SelectedColorAtom);
 
   const handleColorClick = (item: string) => {
+    console.log("++++++", item);
     setColor(item);
     setColorsShow(false);
   };
 
+  const handleMixerColorChange = (value: IColor) => {
+    setMeshColor(value);
+    console.log(">>>>>>>>>>>>>", value);
+    setColor(value.hex);
+  };
   return (
     <MainContainer>
       <IconPallete />
@@ -23,6 +33,9 @@ const ColorsContainer = () => {
           return <Color key={index} backgroundcolor={item} onClick={() => handleColorClick(item)} />;
         })}
       </ColorContainer>
+      <div style={{ zIndex: 1, position: "absolute", top: "100px", right: "5px", visibility: `${colorsShow ? "visible" : "hidden"}` }}>
+        <ColorPicker hideInput={["rgb", "hsv"]} color={color} onChange={handleMixerColorChange} />
+      </div>
     </MainContainer>
   );
 };
